e01f1d361bc9655219e955301efab3fe1c5b839d
Ibrakaaa <ibraka4884@gmail.com>
2022-07-19 10:19:22 +0300

    test

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int k;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen Bir Sayı Giriniz: ");
+        k= input.nextInt();
+
+        for( int i=1;i<=k; i*=4){
+            System.out.println(i);
+
+        }
+        System.out.println("-------------------");
+
+        for(int i=1;i<=k; i*=5){
+            System.out.println(i);
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+

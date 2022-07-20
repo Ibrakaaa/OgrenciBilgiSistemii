329e30ad712550b7ea51d5aa9b4587be832b3734
Ibrakaaa <ibraka4884@gmail.com>
2022-07-20 12:18:49 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,40 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int k;
+        int i,valueNumber,minValue=1,maxValue=1;
+        int number;
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Lütfen Bir Sayı Giriniz: ");
-        k= input.nextInt();
+        System.out.print("Kac Adet Sayi Girmek Istiyorsunuz?: ");
+        valueNumber = input.nextInt();
 
-        for( int i=1;i<=k; i*=4){
-            System.out.println(i);
+        for (i =1; i<= valueNumber;i++){
+            System.out.print("Lutfen "+i+". sayiyi Giriniz: ");
+
+            number = input.nextInt();
+
+            if ( valueNumber ==1){
+
+                System.out.println("Sadece Bir Adet Sayi Girdiniz. En Buyuk ve En Kucuk Degerleri Bulmak " +
+                        "Icin Lutfen Birden Fazla Sayi Giriniz...");
+
+
+            }
+            if(number>maxValue){
+                maxValue = number;
+            }
+            if(number<minValue){
+                minValue =number;
+            }
+
 
         }
-        System.out.println("------------------");
+        System.out.println("En Buyuk Sayi: "+maxValue);
+        System.out.println("En Kucuk Sayi: "+minValue);
 
-        for(int i=1;i<=k; i*=5){
-            System.out.println(i);
 
-        }
+
 
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
